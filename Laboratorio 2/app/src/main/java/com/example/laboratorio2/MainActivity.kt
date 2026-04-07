1bea74be4f28aca1381a2eca10bb3e155ee63ca9
LourdesCabrera-2025 <00034023@uca.edu.sv>
2026-04-06 22:14:45 -0600

    :white_check_mark: Subiendo Laboratorio 2 finalizado

--- Laboratorio 2/app/src/main/java/com/example/laboratorio2/MainActivity.kt
+++ Laboratorio 2/app/src/main/java/com/example/laboratorio2/MainActivity.kt
@@ -1,20 +1,64 @@
 package com.example.laboratorio2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+    private lateinit var nombre: TextInputEditText
+    private lateinit var guardar: Button
+    private lateinit var limpiar: Button
+    private lateinit var tabla: TableLayout
+
+    private val lista = mutableListOf<String>()
+
+    override fun onCreate(savedInstaceState: Bundle?) {
+        super.onCreate(savedInstaceState)
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        nombre = findViewById(R.id.id_nombre)
+        guardar = findViewById(R.id.id_guardar)
+        limpiar = findViewById(R.id.id_limpiar)
+        tabla = findViewById(R.id.id_table)
+
+        guardar.setOnClickListener {
+            val texto = nombre.text.toString()
+            if(texto.isNotEmpty()) {
+                lista.add(texto)
+                agregarFila(texto, lista.size)
+                nombre.text?.clear()
+            }
         }
+
+        limpiar.setOnClickListener {
+            tabla.removeAllViews()
+            lista.clear()
+            nombre.text?.clear()
+        }
+    }
+
+    private fun agregarFila(nombre: String, posicion: Int) {
+        val fila = TableRow(this)
+        val txtNombre = TextView(this)
+        txtNombre.text = nombre
+        txtNombre.textSize = 18f
+        txtNombre.setPadding(20,20,200,20)
+
+        val txtPosicion = TextView(this)
+        txtPosicion.text= posicion.toString()
+        txtPosicion.textSize = 18f
+
+        fila.addView(txtNombre)
+        fila.addView(txtPosicion)
+
+        tabla.addView(fila)
     }
 }
